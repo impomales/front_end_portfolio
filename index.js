f0abb2c4418c1587d92096ee62932158e3e9a083
Isaias <impomales@gmail.com>
2016-09-01 22:19:10 +0000

    shows recent project links

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         e.preventDefault();
 
         $("body, html").animate({ 
-            scrollTop: $( $(this).attr('href') ).offset().top 
+            scrollTop: $( $(this).attr('href') ).offset().top - 100
         }, 600);
     });
 });
